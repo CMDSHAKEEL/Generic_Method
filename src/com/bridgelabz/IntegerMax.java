218cbfcec262bdad7f30a47383790b560f8d3be2
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-06 08:21:01 +0530

    WELCOME TO GENERIC METHOD

---
+++ src/com/bridgelabz/IntegerMax.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class IntegerMax {
+    public static Integer testMax(Integer num1, Integer num2, Integer num3) {
+
+        Integer maximumNumber = num1;
+        if (num2.compareTo(maximumNumber) > 0) {
+            maximumNumber = num2;
+        }
+        if (num3.compareTo(maximumNumber) > 0) {
+            maximumNumber = num3;
+        }
+        return maximumNumber;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter First Numner: ");
+        Integer num1 = scanner.nextInt();
+        System.out.print("Enter Second Numner: ");
+        Integer num2 = scanner.nextInt();
+        System.out.print("Enter Third Numner: ");
+        Integer num3 = scanner.nextInt();
+        Integer maximumNum = testMax(num1, num2, num3);
+        System.out.println("Maximum number is : " + maximumNum);
+    }
+}
